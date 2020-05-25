c1d84900bf05347bdfd1176b9e551586aba5be82
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-05-25 08:22:54 -0500

    original

--- AppCons_test/Program.cs
+++ AppCons_test/Program.cs
@@ -14,32 +14,7 @@ namespace AppCons_test
             Console.WriteLine("Pulse una tecla para continuar...");
             
 
-            //bucle for
-            for(int i=0; i<10; i++)
-            {
-                Console.WriteLine("Hola {0}", i);
-            }
-            Console.ReadKey();
-
-            //bucle for 2
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Saludos {0}", i);
-            }
-            Console.ReadKey();
-
-            //bucle while en rama nueva
-            int j = 0;
-            while (j<10)
-            {
-                j++;
-                Console.WriteLine("Nueva línea {0}", j);
-            }
-
-            //condicionales
-            int a = 10, b = 100;
-            if (a > b)
-                Console.WriteLine("Hola");
+ 
         }
     }
 }
