16981dece903a8ef9489828167502518f684487c
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-05-25 07:47:47 -0500

    Agregar archivos de proyecto.

---
+++ AppCons_test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCons_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo c#");
+            Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
